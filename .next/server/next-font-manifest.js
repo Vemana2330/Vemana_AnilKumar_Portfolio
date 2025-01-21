5850f8ccbf599c14a20993d7216a517b31ea9ca0
Vemana2330 <vemana8341@gmail.com>
2025-01-21 14:04:34 -0500

    Add portfolio files

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,10 +1,6 @@
 self.__NEXT_FONT_MANIFEST={
   "pages": {},
-  "app": {
-    "/Users/vemana/Desktop/Vemana_AnilKumar_Portfolio/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Rubik\",\"arguments\":[{\"weight\":[\"300\",\"400\",\"500\",\"600\",\"700\"],\"style\":[\"normal\"],\"subsets\":[\"latin\"]}],\"variableName\":\"montserrat\"}": [
-      "static/media/c22ccc5eb58b83e1-s.p.woff2"
-    ]
-  },
-  "appUsingSizeAdjust": true,
+  "app": {},
+  "appUsingSizeAdjust": false,
   "pagesUsingSizeAdjust": false
 }
