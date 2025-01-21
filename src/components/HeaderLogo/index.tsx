12823572f1968a7e93fd3848e8cb6265d9bda180
Vemana2330 <vemana8341@gmail.com>
2025-01-21 12:46:10 -0500

    Add portfolio files

--- src/components/HeaderLogo/index.tsx
+++ src/components/HeaderLogo/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Allura } from "next/font/google";
 import AppText from "../AppText";
 import classNames from "classnames";
-//const sacramento = Arial({ weight: "400", subsets: ["latin"] });
+
+// Load the Allura font with fallback to Arial and sans-serif
 const sacramento = Allura({ weight: "400", subsets: ["latin"] });
 
 function HeaderLogo() {
   return (
-    <div className={classNames(sacramento.className, "flex gap-2 shrink-0")}>
+    <div
+      className={classNames(sacramento.className, "flex gap-2 shrink-0")}
+      style={{ fontFamily: "'Allura', Brush Script MT" }} // Added fallback font here
+    >
       <AppText textTag="span" secondary customClass="sm:text-4xl text-3xl">
         {""}
       </AppText>
